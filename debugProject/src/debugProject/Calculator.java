dd96e98dce15ec49b4222a8b27313cd61793eeea
dell <dell@DESKTOP-MS1BQ84>
2025-08-08 14:17:05 +0530

    Initial commit

---
+++ debugProject/src/debugProject/Calculator.java
@@ -0,0 +1,13 @@
+package debugProject;
+
+public class Calculator {
+	
+	void addTwo(int n1, int n2) {
+		
+		//int sum = n1 + n1;
+		int sum = n1 + n2;
+		System.out.println(sum);
+		
+	}
+
+}
---
+++ debugProject/src/debugProject/Test.java
@@ -0,0 +1,12 @@
+package debugProject;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Calculator c1 = new Calculator();
+		c1.addTwo(10, 20);
+		
+	}
+
+}
