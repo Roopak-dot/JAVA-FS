a0d0ee42bae2958bd7d25ee52838c08f11e23b97
dell <roopaksharma000@gmail.com>
2025-08-12 18:32:32 +0530

    Initial commit

---
+++ dataTypeConversion/src/dataTypeConversion/Test.java
@@ -0,0 +1,10 @@
+package dataTypeConversion;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		TypeConversion v1 = new TypeConversion();
+		v1.conversion();
+	}
+}
---
+++ dataTypeConversion/src/dataTypeConversion/TypeConversion.java
@@ -0,0 +1,14 @@
+package dataTypeConversion;
+
+public class TypeConversion {
+	
+	int a = 10;
+	long b = a;
+	int c = (int)b;
+	
+	void conversion() {
+		
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
