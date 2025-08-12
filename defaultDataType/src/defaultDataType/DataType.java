3220e7fe0a9c65950659d8b917c7fc88bc267e1e
dell <roopaksharma000@gmail.com>
2025-08-12 18:24:13 +0530

    Initial commit

---
+++ defaultDataType/src/defaultDataType/DataType.java
@@ -0,0 +1,24 @@
+package defaultDataType;
+
+public class DataType {
+
+	int a;
+	byte b;
+	short c;
+	long d;
+	boolean e;
+	float f;
+	double g;
+	char h;
+	
+	void defaultPrt() {
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+}
---
+++ defaultDataType/src/defaultDataType/Test.java
@@ -0,0 +1,12 @@
+package defaultDataType;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		DataType v1 = new DataType();
+		v1.defaultPrt();
+
+	}
+
+}
